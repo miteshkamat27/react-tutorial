d4428c5e62312b3f8c2b4f47eafc7c75cd599190
Mitesh Kamat <mitesh.kamat@ril.com>
2019-09-05 16:54:35 +0530

    Updates

---
+++ precache-manifest.edc4a6c06ce1c434bce92556e30ce0ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d25cf5ecf9ffcdf18419178a73a330f",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "a78e6271f0be11f14270",
+    "url": "/react-tutorial/static/css/main.71bd5e9e.chunk.css"
+  },
+  {
+    "revision": "396e3a53ca1e8a048e88",
+    "url": "/react-tutorial/static/js/2.fc9cb34d.chunk.js"
+  },
+  {
+    "revision": "a78e6271f0be11f14270",
+    "url": "/react-tutorial/static/js/main.2683f1c6.chunk.js"
+  },
+  {
+    "revision": "91d6c66fbb19dce38e5f",
+    "url": "/react-tutorial/static/js/runtime~main.36beab40.js"
+  }
+]);
