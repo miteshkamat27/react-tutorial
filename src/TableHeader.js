be58d967dc435e3e13d045877d6adc0d73b35e26
Mitesh Kamat <miteshkamat27@gmail.com>
2019-09-05 16:40:28 +0530

    Main commit

---
+++ src/TableHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const TableHeader = () => {
+    return (
+    <thead>
+        <tr>
+            <th>Name</th>
+            <th>Job</th>
+        </tr>
+    </thead>
+    )
+}
